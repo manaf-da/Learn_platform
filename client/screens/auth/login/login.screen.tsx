d1cafae6c35d1c663662e031d3e5f07dab32ff7f
manaf <manafadam@gmail.com>
2024-08-12 13:09:03 +0200

    login ui part 2

--- client/screens/auth/login/login.screen.tsx
+++ client/screens/auth/login/login.screen.tsx
@@ -4,6 +4,7 @@ import {
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
 import {
@@ -27,7 +28,6 @@ import {
 import { useState } from "react";
 import { regularStyles } from "@/styles/regular/regular.style";
 
-
 export default function LoginScreen() {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [buttonSpinner, setButtonSpinner] = useState(false);
@@ -36,13 +36,42 @@ export default function LoginScreen() {
     password: "",
   });
   const [required, setRequired] = useState("");
-  const handlePasswordValidation = (value:string) =>{
-    const password = value
-    const passwordSpecialCharacter = /(?=.*[!@#$&*])/
-    const passwordOneNumber = /(?=.*[0-9])/
-    const passwordSixValue = /(?=.{6,})/
+  const [error, setError] = useState({
+    password: "",
+  });
 
-  }
+  const handlePasswordValidation = (value: string) => {
+    const password = value;
+    const passwordSpecialCharacter = /(?=.*[!@#$&*])/;
+    const passwordOneNumber = /(?=.*[0-9])/;
+    const passwordSixValue = /(?=.{6,})/;
+
+    if (!passwordSpecialCharacter.test(password)) {
+      setError((prev) => ({
+        ...prev,
+        password: "Write password with special character",
+      }));
+      setUserInfo((prev) => ({ ...prev, password: "" }));
+    } else if (!passwordOneNumber.test(password)) {
+      setError((prev) => ({
+        ...prev,
+        password: "Write password with at least One Number",
+      }));
+      setUserInfo((prev) => ({ ...prev, password: "" }));
+    } else if (!passwordSixValue.test(password)) {
+      setError((prev) => ({
+        ...prev,
+        password: "Write password with at least  6 characters",
+      }));
+      setUserInfo((prev) => ({ ...prev, password: "" }));
+    } else {
+      setError((prev) => ({
+        ...prev,
+        password: "",
+      }));
+      setUserInfo((prev) => ({ ...prev, password: value }));
+    }
+  };
 
   return (
     <LinearGradient
@@ -57,22 +86,64 @@ export default function LoginScreen() {
         <Text style={styles.loginText}> Login to your Account</Text>
         <View style={styles.inputContainer}>
           <View>
-            <TextInput style={[styles.input,{paddingLeft:40}]}
-            keyboardType="email-address" value={userInfo.email}
-            placeholder="hmm@gmail.com " onChangeText={(value) => setUserInfo({...userInfo,email:value})}/>
-            <Fontisto style={{position: "absolute", left:26, top:12.8 , paddingLeft:12}}
-            name="email" size={20} color={"A1A1A1"} />
+            <TextInput
+              style={[styles.input, { paddingLeft: 40 }]}
+              keyboardType="email-address"
+              value={userInfo.email}
+              placeholder="hmm@gmail.com "
+              onChangeText={(value) =>
+                setUserInfo({ ...userInfo, email: value })
+              }
+            />
+            <Fontisto
+              style={styles.icon1}
+              name="email"
+              size={20}
+              color={"A1A1A1"}
+            />
             {required && (
-                <View style={regularStyles.errorContainer}>
-                    <Entypo name="cross" size={18} color={"red"}/>
-
-                    </View>
-
+              <View style={regularStyles.errorContainer}>
+                <Entypo name="cross" size={18} color={"red"} />
+              </View>
             )}
-            <View>
-                <TextInput style={regularStyles.input} keyboardType="default" secureTextEntry={!isPasswordVisible} defaultValue=""
-                placeholder="********" onChangeText={handlePasswordValidation} />
+            <View style={{ marginTop: 15 }}>
+              <TextInput
+                style={[styles.input, { paddingLeft: 40 }]}
+                keyboardType="default"
+                secureTextEntry={!isPasswordVisible}
+                defaultValue=""
+                placeholder="********"
+                onChangeText={handlePasswordValidation}
+              />
+              <TouchableOpacity
+                style={styles.visibleIcon}
+                onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+              >
+                {isPasswordVisible ? (
+                  <Ionicons
+                    name="eye-off-outline"
+                    size={26}
+                    color={"#A1A1A1"}
+                  />
+                ) : (
+                  <Ionicons name="eye-outline" size={26} color={"#A1A1A1"} />
+                )}
+              </TouchableOpacity>
+              <SimpleLineIcons
+                style={styles.icon2}
+                name="lock"
+                size={20}
+                color={"#A1A1A1"}
+              />
             </View>
+            {error?.password && (
+              <View style={[regularStyles.errorContainer, { top: 145 }]}>
+                <Entypo name="cross" size={18} color={"red"} />
+                <Text style={{ color: "red", fontSize: 11, marginTop: -1 }}>
+                  {error.password}
+                </Text>
+              </View>
+            )}
           </View>
         </View>
       </ScrollView>
@@ -99,12 +170,29 @@ const styles = StyleSheet.create({
     rowGap: 30,
   },
   input: {
-    height: 40,
+    height: 50,
     marginHorizontal: 35,
     paddingLeft: 40,
     fontSize: 18,
     backgroundColor: "white",
     color: "#000",
     fontFamily: "MerriweatherSans_400Regular",
+  },
+  visibleIcon: {
+    position: "absolute",
+    right: 44,
+    top: 10,
+  },
+  icon1: {
+    position: "absolute",
+    left: 36,
+    top: 16,
+    paddingLeft: 10,
+  },
+  icon2: {
+    position: "absolute",
+    left: 36,
+    top: 16,
+    paddingLeft: 10,
   },
 });
